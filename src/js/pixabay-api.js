1f5a1fb483039a2b49368d94f0cd38d8616538ee
DeStr0y3r90 <98543747+DeStr0y3r90@users.noreply.github.com>
2024-05-20 17:46:45 +0100

    add

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,14 +1,14 @@
+import axios from 'axios';
 
-const KEY = '42879535-23ffe446c86780b92a1736c42';
-const BASE_URL = 'https://pixabay.com/api/';
-
-export function getImages(inputValue) {
-    const LINK = `${BASE_URL}?key=${KEY}&q=${inputValue}`;
-    return fetch(LINK)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('image error!');
-            }
-            return response.json();
-        });
-}
+export const returnPromise = (q, page) => {
+  return axios.get('https://pixabay.com/api/', {
+    params: {
+      key: '42879535-23ffe446c86780b92a1736c42',
+      q,
+      orientation: 'horizontal',
+      safesearch: true,
+      page,
+      per_page: '15',
+    },
+  });
+};
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,18 +1,5 @@
-
-
-import iziToast from 'izitoast';
-
-export function renderImages(images, list) {
-  if (images.length === 0) {
-    iziToast.error({
-      title: '',
-      message:
-        'Sorry, there are no images matching your search query. Please, try again!',
-      position: 'topRight',
-    });
-      return;
-  }
-  const markup = images
+export const returnMarkup = arrSearchPhotos => {
+  const markup = arrSearchPhotos
     .map(
       ({
         webformatURL,
@@ -23,38 +10,31 @@ export function renderImages(images, list) {
         comments,
         downloads,
       }) => {
-        return `<li class="gallery-item">
+        return `<li class="search-list-item">
                     <a class="gallery-link" href="${largeImageURL}">
-                        <img class="gallery-image" src="${webformatURL}" alt="${tags}" />
+                      <img class="search-list-img" src="${webformatURL}" alt="${tags}" />
                     </a>
-                    <div class="parameters">
-                        <div class="params">
-                            <p class="options">Likes</p>
-                            <p class="quantity">${likes}</p>
-                        </div>
-                        <div class="params">
-                            <p class="options">Views</p>
-                            <p class="quantity">${views}</p>
-                        </div>
-                        <div class="params">
-                            <p class="options">Comments</p>
-                            <p class="quantity">${comments}</p>
-                        </div>
-                        <div class="params">
-                            <p class="options">Downloads</p>
-                            <p class="quantity">${downloads}</p>
-                        </div>
-                    </div>
-                </li>`;
+                    <ul>
+                      <li>
+                        <h3>Likes</h3>
+                        <p>${likes}</p>
+                      </li>
+                      <li>
+                        <h3>Views</h3>
+                        <p>${views}</p>
+                      </li>
+                      <li>
+                        <h3>Comments</h3>
+                        <p>${comments}</p>
+                      </li>
+                      <li>
+                        <h3>Downloads</h3>
+                        <p>${downloads}</p>
+                      </li>
+                    </ul>
+                  </li>`;
       }
     )
     .join('');
-  list.insertAdjacentHTML('beforeend', markup);
-}
-
-export function clearList(list, gallery) {
-  list.innerHTML = '';
-  if (gallery) {
-    gallery.refresh();
-  }
-}
+  return markup;
+};
